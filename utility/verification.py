b6af09aa7ac97a0499f0833607c75e2b5c290fb8
Starlight Romero <starlightromero@gmail.com>
2020-10-01 16:34:51 -0700

    removed unecessary statements

--- utility/verification.py
+++ utility/verification.py
@@ -35,14 +35,13 @@ class Verification:
     @staticmethod
     def verify_transaction(transaction, get_balance, check_funds=True):
         """Verify sender has sufficient funds for a given transaction."""
-        if check_funds is True:
+        if check_funds:
             sender_balance = get_balance(transaction.sender)
             return (
                 sender_balance >= transaction.amount
                 and Wallet.verify_transaction(transaction)
             )
-        else:
-            return Wallet.verify_transaction(transaction)
+        return Wallet.verify_transaction(transaction)
 
     @classmethod
     def verify_transactions(cls, open_transactions, get_balance):
