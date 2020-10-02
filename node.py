356336a98631ca1e0a1391cb1602b1996a3dbf76
Starlight Romero <starlightromero@gmail.com>
2020-10-02 12:19:39 -0700

    reverted to send_from_directory

--- node.py
+++ node.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 from wallet import Wallet
@@ -10,12 +10,12 @@ CORS(app)
 
 @app.route("/", methods=["GET"])
 def get_node_ui():
-    return render_template("node.html")
+    return send_from_directory("templates", "node.html")
 
 
 @app.route("/network", methods=["GET"])
 def get_network_ui():
-    return render_template("network.html")
+    return send_from_directory("templates", "network.html")
 
 
 @app.route("/wallet", methods=["POST"])
