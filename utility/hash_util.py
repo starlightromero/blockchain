8bd1a122a04418975d56fa112f348f1611e876c8
Starlight Romero <starlightromero@gmail.com>
2020-10-06 08:16:48 -0700

    updated docstrings

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -4,12 +4,26 @@ import json
 
 
 def has_string_256(string):
-    """Return a 64 character hex string."""
+    """Return a 64 character hex string.
+
+    Arguments:
+    ---------
+        string : str
+            String to be hashed.
+
+    """
     return hashlib.sha256(string).hexdigest()
 
 
 def hash_block(block):
-    """Output a hash for a given input block."""
+    """Output a hash for a given input block.
+
+    Arguments:
+    ---------
+        block
+            Block that to be hashed.
+
+    """
     hashable_block = block.__dict__.copy()
     hashable_block["transactions"] = [
         tx.to_ordered_dict() for tx in hashable_block["transactions"]
